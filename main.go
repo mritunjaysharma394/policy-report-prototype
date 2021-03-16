2dab38fe7f784f58880b4b04571b8a8c0a7ce972
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-03-17 02:07:12 +0530

    initial step to print json

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
+// package
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	//calls function that runs KubeBench
+	out := runKubeBench()
+	fmt.Println(out)
+}
+
+func runKubeBench() string {
+
+	//executes kube-bench
+	cmd := exec.Command("./kube-bench", "--json")
+	out, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(out)
 }
