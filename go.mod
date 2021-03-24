cb195e6bfcb88a2228c31854d1582156eca618c0
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-03-25 00:57:33 +0530

    refined the json struct using kube-bench schema

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mritunjaysharma394/policy-report-prototype
 
 go 1.16
+
+require sigs.k8s.io/yaml v1.2.0 // indirect
--- main.go
+++ main.go
@@ -11,56 +11,151 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-type Body struct {
-	Controls []Control `json:"Controls,omitempty"`
-	Totals   Total     `json:"Totals,omitempty"`
+type OverallControls struct {
+	Controls []*Controls
+	Totals   Summary
 }
 
-type Control struct {
-	ID        string `json:"id,omitempty"`
-	Version   string `json:"version,omitempty"`
-	Text      string `json:"text,omitempty"`
-	NodeType  string `json:"node_type,omitempty"`
-	Tests     []Test `json:"tests,omitempty"`
-	TotalPass int    `json: "total_pass,omitempty"`
-	TotalFail int    `json: "total_fail,omitempty"`
-	TotalWarn int    `json: "total_warn,omitempty"`
-	TotalInfo int    `json: "total_info,omitempty"`
+// Controls holds all controls to check for master nodes.
+type Controls struct {
+	ID      string   `yaml:"id" json:"id"`
+	Version string   `json:"version"`
+	Text    string   `json:"text"`
+	Type    NodeType `json:"node_type"`
+	Groups  []*Group `json:"tests"`
+	Summary
 }
 
-type Test struct {
-	Section string   `json: "section,omitempty"`
-	Type    string   `json: "type,omitempty"`
-	Pass    int      `json: "pass,omitempty"`
-	Fail    int      `json: "fail,omitempty"`
-	Warn    int      `json: "warn,omitempty"`
-	Info    int      `json: "info,omitempty"`
-	Desc    string   `json: "desc,omitempty"`
-	Results []Result `json: "results,omitempty"`
+// Group is a collection of similar checks.
+type Group struct {
+	ID     string   `yaml:"id" json:"section"`
+	Type   string   `yaml:"type" json:"type"`
+	Pass   int      `json:"pass"`
+	Fail   int      `json:"fail"`
+	Warn   int      `json:"warn"`
+	Info   int      `json:"info"`
+	Text   string   `json:"desc"`
+	Checks []*Check `json:"results"`
 }
 
-type Result struct {
-	TestNumber  string `json: "test_number"`
-	TestDesc    string `json: "test_desc,omitempty"`
-	Audit       string `json: "audit,omitempty"`
-	AuditEnv    string `json: "AuditEnv,omitempty"`
-	AuditConfig string `json: "AuditConfig,omitempty"`
-	Type        string `json: "type,omitempty"`
-	Remediation string `json: "remediation, omitempty"`
-	// test_info has to be fixed
-	//TestInfo []TestInformation `json: "test_info,omitempty"`
-	Status         string `json: "status,omitempty"`
-	ActualValue    string `json: "actual_value,omitempty"`
-	Scored         bool   `json: "scored,omitempty"`
-	IsMultiple     bool   `json: "IsMultiple,omitempty"`
-	ExpectedResult string `json: "expected_result,omitempty"`
+// Summary is a summary of the results of control checks run.
+type Summary struct {
+	Pass int `json:"total_pass"`
+	Fail int `json:"total_fail"`
+	Warn int `json:"total_warn"`
+	Info int `json:"total_info"`
 }
 
-type Total struct {
-	TotalPass int `json: "total_pass,omitempty"`
-	TotalFail int `json: "total_fail,omitempty"`
-	TotalWarn int `json: "total_warn,omitempty"`
-	TotalInfo int `json: "total_info,omitempty"`
+// Predicate a predicate on the given Group and Check arguments.
+type Predicate func(group *Group, check *Check) bool
+
+// NodeType indicates the type of node (master, node).
+type NodeType string
+
+// State is the state of a control check.
+type State string
+
+const (
+	// PASS check passed.
+	PASS State = "PASS"
+	// FAIL check failed.
+	FAIL State = "FAIL"
+	// WARN could not carry out check.
+	WARN State = "WARN"
+	// INFO informational message
+	INFO State = "INFO"
+
+	// SKIP for when a check should be skipped.
+	SKIP = "skip"
+
+	// MASTER a master node
+	MASTER NodeType = "master"
+	// NODE a node
+	NODE NodeType = "node"
+	// FEDERATED a federated deployment.
+	FEDERATED NodeType = "federated"
+
+	// ETCD an etcd node
+	ETCD NodeType = "etcd"
+	// CONTROLPLANE a control plane node
+	CONTROLPLANE NodeType = "controlplane"
+	// POLICIES a node to run policies from
+	POLICIES NodeType = "policies"
+	// MANAGEDSERVICES a node to run managedservices from
+	MANAGEDSERVICES = "managedservices"
+
+	// MANUAL Check Type
+	MANUAL string = "manual"
+)
+
+// Check contains information about a recommendation in the
+// CIS Kubernetes document.
+type Check struct {
+	ID                string   `yaml:"id" json:"test_number"`
+	Text              string   `json:"test_desc"`
+	Audit             string   `json:"audit"`
+	AuditEnv          string   `yaml:"audit_env"`
+	AuditConfig       string   `yaml:"audit_config"`
+	Type              string   `json:"type"`
+	Tests             *tests   `json:"-"`
+	Set               bool     `json:"-"`
+	Remediation       string   `json:"remediation"`
+	TestInfo          []string `json:"test_info"`
+	State             `json:"status"`
+	ActualValue       string `json:"actual_value"`
+	Scored            bool   `json:"scored"`
+	IsMultiple        bool   `yaml:"use_multiple_values"`
+	ExpectedResult    string `json:"expected_result"`
+	Reason            string `json:"reason,omitempty"`
+	AuditOutput       string `json:"-"`
+	AuditEnvOutput    string `json:"-"`
+	AuditConfigOutput string `json:"-"`
+	DisableEnvTesting bool   `json:"-"`
+}
+
+type binOp string
+
+const (
+	and                   binOp = "and"
+	or                          = "or"
+	defaultArraySeparator       = ","
+)
+
+type tests struct {
+	TestItems []*testItem `yaml:"test_items"`
+	BinOp     binOp       `yaml:"bin_op"`
+}
+
+type AuditUsed string
+
+const (
+	AuditCommand AuditUsed = "auditCommand"
+	AuditConfig  AuditUsed = "auditConfig"
+	AuditEnv     AuditUsed = "auditEnv"
+)
+
+type testItem struct {
+	Flag             string
+	Env              string
+	Path             string
+	Output           string
+	Value            string
+	Set              bool
+	Compare          compare
+	isMultipleOutput bool
+	auditUsed        AuditUsed
+}
+
+type compare struct {
+	Op    string
+	Value string
+}
+
+type testOutput struct {
+	testResult     bool
+	flagFound      bool
+	actualResult   string
+	ExpectedResult string
 }
 
 func main() {
@@ -70,7 +165,7 @@ func main() {
 	jsonDataReader := strings.NewReader(out)
 	decoder := json.NewDecoder(jsonDataReader)
 
-	var body Body
+	var body OverallControls
 	err := decoder.Decode(&body)
 	if err != nil {
 		panic(err)
@@ -86,10 +181,10 @@ func main() {
 	// }
 
 	fmt.Println(body.Controls[0].ID)
-	fmt.Println(body.Controls[1].TotalPass) //not showing correct output
-	fmt.Println(body.Controls[0].Tests[0].Fail)
-	fmt.Println(body.Controls[0].Tests[0].Results[0].Status)
-	fmt.Println(body.Totals.TotalPass) // not showing correct output
+	fmt.Println(body.Controls[1].Summary) //not showing correct output
+	// fmt.Println(body.Controls[0].Tests[0].Fail)
+	// fmt.Println(body.Controls[0].Tests[0].Results[0].Status)
+	// fmt.Println(body.Totals.TotalPass) // not showing correct output
 
 	// Prints entire json as yaml (Caution: A few fields are buggy, to be fixed)
 	y, err := yaml.Marshal(body)
