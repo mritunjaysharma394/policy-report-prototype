b9b76f23ddfc84d30a49b21f10b87bc052f748bf
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-04-16 02:05:25 +0530

    adds unit test for createPolicyReport

--- main_test.go
+++ main_test.go
@@ -15,22 +15,56 @@ func TestCreatePolicyReport(t *testing.T) {
 		name         string
 		policyreport *appsv1aplha1.PolicyReport
 		ns           string
-	}{{"demo-test", &appsv1aplha1.PolicyReport{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo",
-		},
-		Summary: appsv1aplha1.PolicyReportSummary{
-			Pass: 10,
-			Fail: 4,
-			Warn: 0,
-		},
-	}, "default"},
+	}{
+		{"demo-test-1", &appsv1aplha1.PolicyReport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "demo",
+			},
+			Summary: appsv1aplha1.PolicyReportSummary{
+				Pass: 10,
+				Fail: 4,
+				Warn: 0,
+			},
+		}, "default"},
+		{"demo-test-2", &appsv1aplha1.PolicyReport{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "demo",
+			},
+			Summary: appsv1aplha1.PolicyReportSummary{
+				Pass: 5,
+				Fail: 4,
+				Warn: 0,
+			},
+			Results: []*appsv1aplha1.PolicyReportResult{
+				{
+					Policy:      "test-policy",
+					Rule:        "test-rule",
+					Category:    "test-category",
+					Result:      "pass",
+					Scored:      true,
+					Description: "test-description",
+					Properties: map[string]string{
+						"index":           "1",
+						"audit":           "",
+						"AuditEnv":        "",
+						"AuditConfig":     "",
+						"type":            "test-type",
+						"remediation":     "test-remediation",
+						"test_info":       "test",
+						"actual_value":    "test-actual-value",
+						"IsMultiple":      "true",
+						"expected_result": "test-exp-result",
+						"reason":          "test-reason",
+					},
+				},
+			},
+		}, "default"},
 	}
 
 	for _, pr := range policyTests {
 		_, err := testclient.NewSimpleClientset().Wgpolicyk8sV1alpha1().PolicyReports(pr.ns).Create(context.TODO(), pr.policyreport, metav1.CreateOptions{})
 		if err != nil {
-			t.Fatalf("error injecting pod add: %v", err)
+			t.Fatalf("error creating policy report: %v", err)
 		}
 	}
 }
