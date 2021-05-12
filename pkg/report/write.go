383d552e86d0d7fb98e2c94e6a6575b11449c7a2
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-05-12 14:27:36 +0530

    fixes updating policy reports with same name

--- pkg/report/write.go
+++ pkg/report/write.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	// TODO: upgrade to v1alpha2 CRD and create a Makefile / shell script for code generation
-	// see example at: https://leftasexercise.com/2019/07/29/building-a-bitcoin-controller-for-kubernetes-part-ii/
-	// https://github.com/christianb93/bitcoin-controller/blob/master/build/controller/generate_code.sh
 	policyreport "github.com/mritunjaysharma394/policy-report-prototype/pkg/apis/wgpolicyk8s.io/v1alpha2"
 
 	client "github.com/mritunjaysharma394/policy-report-prototype/pkg/generated/v1alpha2/clientset/versioned"
@@ -28,31 +25,29 @@ func Write(r *policyreport.PolicyReport, namespace string, kubeconfig string) (*
 
 	policyReport := clientset.Wgpolicyk8sV1alpha2().PolicyReports(namespace)
 
-	fmt.Println("Creating Policy Report...")
+	// Check for existing Policy Reports
+	result, getErr := policyReport.Get(context.TODO(), r.Name, metav1.GetOptions{})
+	// Create new Policy Report if not found
+	if getErr != nil {
+		fmt.Println("creating policy report...")
 
-	result, err := policyReport.Create(context.TODO(), r, metav1.CreateOptions{})
-	if err != nil {
-		return nil, err
-	}
-	// TODO : Get first
-
-	// Update Policy Report
-	fmt.Println("Updating Policy Report...")
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		// Retrieve the latest version of Policy Report before attempting update
-		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-		result, getErr := policyReport.Get(context.TODO(), r.Name, metav1.GetOptions{})
-		if getErr != nil {
-			panic(fmt.Errorf("failed to get latest version of Policy Report: %v", getErr))
+		result, err = policyReport.Create(context.TODO(), r, metav1.CreateOptions{})
+		if err != nil {
+			return nil, err
 		}
-		// if (result.Name!=
-		_, updateErr := policyReport.Update(context.TODO(), result, metav1.UpdateOptions{})
-		return updateErr
-	})
-	if retryErr != nil {
-		panic(fmt.Errorf("update failed: %v", retryErr))
+	} else {
+
+		// Update existing Policy Report
+		fmt.Println("updating policy report...")
+		retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			_, updateErr := policyReport.Update(context.TODO(), result, metav1.UpdateOptions{})
+			return updateErr
+		})
+		if retryErr != nil {
+			panic(fmt.Errorf("update failed: %v", retryErr))
+		}
+		fmt.Println("updated policy report...")
 	}
-	fmt.Println("Updated Policy Report...")
 
 	return result, nil
 }
