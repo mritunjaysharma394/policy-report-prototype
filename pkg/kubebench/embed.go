12e829f50ce6edf83b10cc9875c6b2d4f9cfc391
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-05-13 21:20:44 +0530

    cleans up embed code

--- pkg/kubebench/embed.go
+++ pkg/kubebench/embed.go
@@ -13,17 +13,6 @@ func embedYAMLs(kubebenchYAML string) ([]byte, error) {
 		data []byte
 		err  error
 	)
-
-	// jobs, _ := fs.ReadDir(yamlDir, "jobs")
-
-	// for _, job := range jobs {
-	// 	if kubebenchYAML == job.Name() {
-	// 		data, err = yamlDir.ReadFile("jobs/" + job.Name())
-	// 	} else {
-	// 		continue
-	// 	}
-	// }
-
 	data, err = yamlDir.ReadFile("jobs/" + kubebenchYAML)
 	if err != nil {
 		return nil, err
